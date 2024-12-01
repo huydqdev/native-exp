37556ac9880095a1aca9206e3aec3bd1d4bfd7dc
huydqdev <huydq.tech@stepup.edu.vn>
2024-12-02 01:48:14 +0700

    update: add native provider

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { View, StyleSheet } from "react-native";
+import { NativeBaseProvider, Text, Box } from "native-base";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screennnn.</Text>
+    <View style={styles.container}>
+        <NativeBaseProvider>
+            <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+                <Text>Open up App.js to start working on your app!</Text>
+            </Box>
+        </NativeBaseProvider>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+  }
+})
