d4fb11ecc6fc8fe1925220e66ead22716ce904a0
huydqdev <huydq.tech@stepup.edu.vn>
2024-12-04 00:46:17 +0700

    feat: config tools

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { View, StyleSheet } from "react-native";
-import { NativeBaseProvider, Text, Box } from "native-base";
+import { View, StyleSheet,Text } from "react-native";
+import { store } from 'store'
+import { Provider } from 'react-redux'
 
 export default function Index() {
   return (
     <View style={styles.container}>
-        <NativeBaseProvider>
-            <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-                <Text>Open up App.js to start working on your app!</Text>
-            </Box>
-        </NativeBaseProvider>
+        <Provider store={store}>
+            <Text>Need check here </Text>
+        </Provider>
     </View>
   );
 }
@@ -17,6 +16,7 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   container: {
+      backgroundColor: 'lightblue',
       flex: 1,
       justifyContent: "center",
       alignItems: "center",
