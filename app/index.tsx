6e6fdd11b34728449861a258498157988d0ffbf2
huydqdev <huydq.tech@stepup.edu.vn>
2024-12-05 01:30:29 +0700

    feat: config firebase

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { View, StyleSheet,Text } from "react-native";
-import { store } from 'store'
+import { store } from '@/app/store/store'
 import { Provider } from 'react-redux'
+import Login from './components/authen/login';
 
 export default function Index() {
   return (
     <View style={styles.container}>
-        <Provider store={store}>
-            <Text>Need check here </Text>
-        </Provider>
+        {/*<Provider store={store}>*/}
+            <Text>Need check heree </Text>
+            <Login />
+        {/*</Provider>*/}
     </View>
   );
 }
